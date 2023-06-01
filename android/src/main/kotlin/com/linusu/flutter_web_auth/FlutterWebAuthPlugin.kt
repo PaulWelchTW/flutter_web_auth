8e387316612c47153c0be685fe6b9850afb6bf3c
pwelch <paul.welch@templeandwebster.com.au>
2023-06-01 13:10:29 +1000

    Include missing import.

--- android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsSession
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
